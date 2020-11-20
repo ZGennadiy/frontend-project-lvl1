8955b4ce665a83c03e7fc32349159418b489ead6
ZGennadiy <gennadiy.zinchenko@gmail.com>
2020-11-20 12:11:04 +0300

    add greeting by name

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
+import { greetByName } from "../src/cli.js";
+
+// const { greetByName } = require("../src/cli");
+
 console.log("Welcome to the Brain Games!");
+
+greetByName();

3571fc64a99189b7f38a19206ba0316ea0f7c3a5
ZGennadiy <gennadiy.zinchenko@gmail.com>
2021-01-04 13:13:34 +0300

    fix linter errors

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import { greetByName } from "../src/cli.js";
+import { greetByName } from '../src/cli.js';
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 
 greetByName();

83556924afea15723d9a3f4df92ae3ea0155d7cc
ZGennadiy <gennadiy.zinchenko@gmail.com>
2021-01-12 23:58:14 +0300

    refactoring for brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
 import greetByName from '../src/cli.js';
+import evenGame from '../src/even-game.js';
 
 console.log('Welcome to the Brain Games!');
 
@@ -9,30 +9,4 @@ const userName = greetByName();
 
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-const isEven = (num) => num % 2 === 0;
-const getRandomNumber = (maxNumber = 100) => Math.ceil(Math.random() * maxNumber);
-const isCorrectAnswer = (num, answer) => (isEven(num) && answer.toLowerCase() === 'yes')
-  || (!isEven(num) && answer.toLowerCase() === 'no');
-const getCorrectAnswer = (num) => (isEven(num) ? 'yes' : 'no');
-
-const evenGame = () => {
-  for (let i = 0; i < 3; i += 1) {
-    const randomNumber = getRandomNumber();
-    console.log(`Question: ${randomNumber}`);
-    const answer = readlineSync.question('Your answer: ');
-
-    if (isCorrectAnswer(randomNumber, answer)) {
-      console.log('Correct!');
-    } else {
-      console.log(`${answer} is wrong answer ;(. Correct answer was '${getCorrectAnswer(
-        randomNumber,
-      )}'.
-      Let's try again, ${userName}!`);
-      return;
-    }
-  }
-
-  console.log(`Congratulations, ${userName}!`);
-};
-
-evenGame();
+evenGame(userName);
---
+++ src/even-game.js
@@ -0,0 +1,34 @@
+import readlineSync from 'readline-sync';
+import greetByName from './cli.js';
+
+const isEven = (num) => num % 2 === 0;
+const getRandomNumber = (maxNumber = 100) => Math.ceil(Math.random() * maxNumber);
+const isCorrectAnswer = (num, answer) => (isEven(num) && answer.toLowerCase() === 'yes')
+  || (!isEven(num) && answer.toLowerCase() === 'no');
+const getCorrectAnswer = (num) => (isEven(num) ? 'yes' : 'no');
+
+const evenGame = () => {
+  const userName = greetByName();
+
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  for (let i = 0; i < 3; i += 1) {
+    const randomNumber = getRandomNumber();
+    console.log(`Question: ${randomNumber}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    if (isCorrectAnswer(randomNumber, answer)) {
+      console.log('Correct!');
+    } else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was '${getCorrectAnswer(
+        randomNumber,
+      )}'.
+      Let's try again, ${userName}!`);
+      return;
+    }
+  }
+
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default evenGame;
